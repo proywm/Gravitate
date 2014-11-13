11e7d72a3034396a7d01227d502908c3754c7b78
Dylan <dahysen@email.wm.edu>
2014-11-13 15:36:32 -0500

    Line Deletion Implemented
    Works in all 4 directions

--- include/GameOwner.h
+++ include/GameOwner.h
@@ -1,65 +1,480 @@
-#pragma once
-#include "GamePlayer.h"
-#include "GlobalHeader.h"
-#include "Actor.h"
+#include "GameOwner.h"
+#include "ActorComponentInterface.h"
+#include "GameLogicManager.h"
+#include "PhysicalComponent.h"
+#include "VisualComponent.h"
+#include "DisplayManager.h"
+#include "GameStateComponent.h"
+#include "SelectionToolBarComponent.h"
+#include <math.h>
+#include <sstream>
 
-enum Direction 
+GameOwner::GameOwner(void)
 {
-	SOUTH = 0,
-	EAST = 1,
-	NORTH = 2,
-	WEST = 3,
-};
-
-
-class GameOwner :
-	public GamePlayer
+}
+GameOwner::~GameOwner(void)
 {
-public:
-	GameOwner(void);
-	~GameOwner(void);
+}
+void GameOwner::init(const char* actorsList)
+{
+	XMLDocument* doc = new XMLDocument();
+    	doc->LoadFile(actorsList);
+	XMLElement *playerFiles = doc->FirstChildElement();
+	ConfiguredSHIFTTIME = playerFiles->IntAttribute("shiftTime");
+	ConfiguredGAMETIME = playerFiles->IntAttribute("gameTime");
 
-	void init(const char* actorsList);
-	void update(double deltaMS);
-	void HandleEvent(sf::Event receivedEvent);
+	font.loadFromFile("./src/arial.ttf");
+	maxShift = 15;
 
-	void controlGame(void);
-	void startGame(void);
-	void StopGame();
-	void ShowWinner(GamePlayer* winner);
-	void updateScoreView();
-	void RestartGame();
+	MaxPossibleScore = 10;//hardcoded
+	srand(1);
+	direction[0]= randomGravity();
+	direction[1]= randomGravity();
+	direction[2]= randomGravity();
+	direction[3]= randomGravity();
 
-	void ShiftLeftRequest(int shapeId);
-	void ShiftRightRequest(int shapeId);
-	void ShiftUpRequest(int shapeId);
-	void ShiftDownRequest(int shapeId);
-	void RotateCounterClockwiseRequest(int shapeId);
-	void RotateClockwiseRequest(int shapeId);
-
-	void CreateShapeRequest();
-	int SelectedShape();
-	int randomShapeSelection();
-	void SelectShapeRequest();
-
-	void ImplementGravity(double deltaMS);
-	bool ismoveableBlock(int blockId);
-
-	int MaxPossibleScore;
-	int maxShift;
-	Direction direction[4];
-	GamePlayer* winner;
-	bool HasWinner;
+	gameTime = 0; 
+	shiftTime = 0;
+	HasWinner = false;
+	controlGame();
+}
+void GameOwner::update(double deltaMS)
+{	
+	ImplementGravity(deltaMS);
 	
-	Direction randomGravity();
-	Direction getDirection();
-	void LineDeletion();
+	//Check for Line Deletion
+	LineDeletion();
 	
-	int shiftTime;
-	int gameTime;
+	if(HasWinner)
+		controlGame();
+	
+	updateScoreView();
+}
+bool GameOwner::ismoveableBlock(int blockId)
+{
+	if (blockId != BOUNDARYIDENTIFIER && blockId != EMPTYBLOCK && blockId != CONCRETEBLOCK)
+		return true;
+	else
+		return false;
+}
+void GameOwner::ImplementGravity(double deltaMS)
+{
+	for(actorIterType iter = actorMap.begin(); iter != actorMap.end(); ++iter)
+	{
+		Actor* actor = (Actor*)iter->second;
+		if(actor->actorType == "Map")
+		{	
 
-	sf::Font font;
+			gameTime += deltaMS;
+			if (gameTime >= ConfiguredGAMETIME)
+			{
+				gameTime = 0;
+				shiftTime += 1000;
+				if (shiftTime >= ConfiguredSHIFTTIME)
+				{
+					shiftTime = 0;
+					for (int i = 0; i < 3; i++)
+					{
+						direction[i] = direction[i+1];
+					}
+					direction[3] = randomGravity();
+				}
+				GameStateComponent* gameStateComponent = (GameStateComponent*)actor->GetComponent(GAMESTATE);	
+
+				 switch(direction[0])
+				 {
+				   case SOUTH:
+					for(int r=MAXROW-2;r>=0;r--)
+					{
+						for(int c=0;c<MAXCOL;c++)
+						{
+							//calling proper function to shift non-empty block
+							if (ismoveableBlock(gameStateComponent->GameMap[r][c]))
+								ShiftDownRequest(gameStateComponent->GameMap[r][c]);
+
+						}
+					}
+					break;
+					
+				   case EAST: 
+					for(int r=0;r<MAXROW;r++)
+					{
+						for(int c=MAXCOL-2;c>=0;c--)
+						{
+							//calling proper function to shift non-empty block
+							if (ismoveableBlock(gameStateComponent->GameMap[r][c]))
+								ShiftRightRequest(gameStateComponent->GameMap[r][c]);
+						}
+					}
+					break;
+				   case NORTH:
+					for(int r=1;r<MAXROW;r++)
+					{
+						for(int c=0;c<MAXCOL;c++)
+						{
+							//calling proper function to shift non-empty block
+							if (ismoveableBlock(gameStateComponent->GameMap[r][c]))
+								ShiftUpRequest(gameStateComponent->GameMap[r][c]);
+						}
+					}
+					break;
+				   case WEST:
+					for(int r=0;r<MAXROW;r++)
+					{
+						for(int c=1;c<MAXCOL;c++)
+						{
+							//calling proper function to shift non-empty block
+							if (ismoveableBlock(gameStateComponent->GameMap[r][c]))
+								ShiftLeftRequest(gameStateComponent->GameMap[r][c]);
+						}
+					}
+					break;
+				   default:
+				     break;
+				}
+			}
+		}
+	}
+}
+void GameOwner::HandleEvent(sf::Event receivedEvent)
+{
+	if(receivedEvent.key.code == sf::Keyboard::Q)
+	{
+		DisplayManager::instance()->window.close();
+	}
+	else if(receivedEvent.key.code == sf::Keyboard::R)
+	{
+		RestartGame();
+	}
+}
+void GameOwner::ShiftLeftRequest(int shapeId)
+{
+	for(actorIterType iter = actorMap.begin(); iter != actorMap.end(); ++iter)
+	{
+		Actor* actor = (Actor*)iter->second;
+		if(actor->actorType == "Map")
+		{
+			GameStateComponent* gameStateComponent = (GameStateComponent*)actor->GetComponent(GAMESTATE);
+			//hardcoded shapes; ToDo: Make dynamic
+			gameStateComponent->ShiftLeft(shapeId);
+		}
+	}
+}
+void GameOwner::ShiftRightRequest(int shapeId)
+{
+	for(actorIterType iter = actorMap.begin(); iter != actorMap.end(); ++iter)
+	{
+		Actor* actor = (Actor*)iter->second;
+		if(actor->actorType == "Map")
+		{
+			GameStateComponent* gameStateComponent = (GameStateComponent*)actor->GetComponent(GAMESTATE);
+			//hardcoded shapes; ToDo: Make dynamic
+			gameStateComponent->ShiftRight(shapeId);
+		}
+	}
+}
+void GameOwner::ShiftUpRequest(int shapeId)
+{
+	for(actorIterType iter = actorMap.begin(); iter != actorMap.end(); ++iter)
+	{
+		Actor* actor = (Actor*)iter->second;
+		if(actor->actorType == "Map")
+		{
+			GameStateComponent* gameStateComponent = (GameStateComponent*)actor->GetComponent(GAMESTATE);
+			//hardcoded shapes; ToDo: Make dynamic
+			gameStateComponent->ShiftUp(shapeId);
+		}
+	}
+}
+void GameOwner::ShiftDownRequest(int shapeId)
+{
+	for(actorIterType iter = actorMap.begin(); iter != actorMap.end(); ++iter)
+	{
+		Actor* actor = (Actor*)iter->second;
+		if(actor->actorType == "Map")
+		{
+			GameStateComponent* gameStateComponent = (GameStateComponent*)actor->GetComponent(GAMESTATE);
+			//hardcoded shapes; ToDo: Make dynamic
+			gameStateComponent->ShiftDown(shapeId);
+		}
+	}
+}
+void GameOwner::RotateCounterClockwiseRequest(int shapeId)
+{
+	for(actorIterType iter = actorMap.begin(); iter != actorMap.end(); ++iter)
+	{
+		Actor* actor = (Actor*)iter->second;
+		if(actor->actorType == "Map")
+		{
+			GameStateComponent* gameStateComponent = (GameStateComponent*)actor->GetComponent(GAMESTATE);
+			//hardcoded shapes; ToDo: Make dynamic
+			gameStateComponent->RotateCounterClockwise(shapeId);
+		}
+	}
+}
+void GameOwner::RotateClockwiseRequest(int shapeId)
+{
+	for(actorIterType iter = actorMap.begin(); iter != actorMap.end(); ++iter)
+	{
+		Actor* actor = (Actor*)iter->second;
+		if(actor->actorType == "Map")
+		{
+			GameStateComponent* gameStateComponent = (GameStateComponent*)actor->GetComponent(GAMESTATE);
+			//hardcoded shapes; ToDo: Make dynamic
+			gameStateComponent->RotateClockwise(shapeId);
+		}
+	}
+}
+int GameOwner::randomShapeSelection()
+{
+	TetrominoShape tetrominoShape = (TetrominoShape)(rand()% ((int)TOTALSHAPES-1)+1);
+	return tetrominoShape;
+}
+int GameOwner::SelectedShape()
+{
+//	return randomShapeSelection();
+	for(actorIterType iter = actorMap.begin(); iter != actorMap.end(); ++iter)
+	{
+		Actor* actor = (Actor*)iter->second;
+		if(actor->actorType == "ShapeSelectionToolBar")
+		{
+			SelectionToolBarComponent* selectionToolBarComponent = (SelectionToolBarComponent*)actor->GetComponent(SELECTIONTOOLBAR);
+			return selectionToolBarComponent->GetSelectedShape();
+		}
+	}
+	return -1;
+}
+void GameOwner::CreateShapeRequest()
+{
+	for(actorIterType iter = actorMap.begin(); iter != actorMap.end(); ++iter)
+	{
+		Actor* actor = (Actor*)iter->second;
+		if(actor->actorType == "Map")
+		{
+			GameStateComponent* gameStateComponent = (GameStateComponent*)actor->GetComponent(GAMESTATE);
+			PhysicalComponent* physicalComponent = (PhysicalComponent*)actor->GetComponent(PHYSICAL);
+			int blockSize = ((ActorShape::GridMap*)physicalComponent->actorShape)->blockSize;
+			//Get mouse position in window
+			sf::Vector2i localPosition = sf::Mouse::getPosition(DisplayManager::instance() -> window);
+			
+			//Convert position to grid
+			int xGridPosition = floor((localPosition.x - physicalComponent->getActorPosition().x)/blockSize);
+			int yGridPosition = floor((localPosition.y - physicalComponent->getActorPosition().x)/blockSize);
+			
+			int shapeId = SelectedShape();
+			if(shapeId!=-1)//something seleceted check
+				gameStateComponent->CreateNewShape((TetrominoShape)shapeId, yGridPosition, xGridPosition);
+			else
+				printf("Nothing Selected");
+		}
+	}
+}
+void GameOwner::SelectShapeRequest()
+{
+	for(actorIterType iter = actorMap.begin(); iter != actorMap.end(); ++iter)
+	{
+		Actor* actor = (Actor*)iter->second;
+		if(actor->actorType == "ShapeSelectionToolBar")
+		{
+			SelectionToolBarComponent* selectionToolBarComponent = (SelectionToolBarComponent*)actor->GetComponent(SELECTIONTOOLBAR);
+			PhysicalComponent* physicalComponent = (PhysicalComponent*)actor->GetComponent(PHYSICAL);
+			int blockSize = ((ActorShape::GridMap*)physicalComponent->actorShape)->blockSize;
+			//Get mouse position in window
+			sf::Vector2i localPosition = sf::Mouse::getPosition(DisplayManager::instance() -> window);
+		
+			//Convert position to grid
+			int xGridPosition = floor((localPosition.x - physicalComponent->getActorPosition().x)/blockSize);
+			int yGridPosition = floor((localPosition.y - physicalComponent->getActorPosition().y)/blockSize);
+			
+			selectionToolBarComponent->SelectShape(yGridPosition, xGridPosition);
+		}
+	}
+}
+
+void GameOwner::controlGame(void)
+{
+	//Hard Coded: ToDO: Make Dynamic Game Control
+	if(HasWinner)
+	{
+		ShowWinner(winner);
+		StopGame();
+	}
+	else
+	{
+		startGame();
+	}
+}
+void GameOwner::startGame(void)
+{
 	
-	int ConfiguredSHIFTTIME;
-	int ConfiguredGAMETIME;
-};
+}
+void GameOwner::StopGame()
+{
+	
+}
+void GameOwner::ShowWinner(GamePlayer* winner)
+{
+	
+}
+void GameOwner::updateScoreView()
+{
+	
+}
+void GameOwner::RestartGame()
+{
+	
+	controlGame();
+}
+
+Direction GameOwner::randomGravity()
+{
+	int dirr = rand() % 4;
+	if (dirr == 0)
+		return (SOUTH); //SOUTH
+	else if (dirr == 1)
+		return (EAST);//EAST
+	else if (dirr == 2)
+		return (NORTH); //NORTH
+	else
+		return (WEST); //WEST
+}
+Direction GameOwner::getDirection()
+{
+  return (direction[0]);
+}
+
+void GameOwner::LineDeletion()
+{  
+  bool topRowFlag = false;
+  bool botRowFlag = false;
+  bool topColFlag = false;
+  bool botColFlag = false;
+
+  for(actorIterType iter = actorMap.begin(); iter != actorMap.end(); ++iter)
+	{
+		Actor* actor = (Actor*)iter->second;
+		if(actor->actorType == "Map")
+		{
+			GameStateComponent* gameStateComponent = (GameStateComponent*)actor->GetComponent(GAMESTATE);	
+ 
+		  //Check top col. If full: delete top col. LEFT.
+		  for (int r=0; r < gameStateComponent->CurrentGameRow;r++)
+		  {
+		    if(gameStateComponent->GameMap[r][0] < 1)
+		    {
+		      topColFlag = true;
+		    }
+		  }
+		  
+		  if(topColFlag == false)
+		  {
+		    std::cout<<"LINEDELETION";
+		    for (int c=1; c < gameStateComponent->CurrentGameRow;c++)
+		    {
+		      for (int r=0; r < gameStateComponent->CurrentGameCol;r++)
+		      {
+				gameStateComponent->GameMap[r][c-1] = gameStateComponent->GameMap[r][c];
+		      }
+		    }
+		    
+		    for (int i=0; i < gameStateComponent->CurrentGameCol; i++)
+		    {
+		      gameStateComponent->GameMap[i][gameStateComponent->CurrentGameCol] = 0;
+		    }
+		  }
+			
+			
+		  
+		  //Check bottom col. If full: delete bottom col. RIGHT.
+		  for (int r=0; r < gameStateComponent->CurrentGameRow;r++)
+		  {
+		    if(gameStateComponent->GameMap[r][gameStateComponent->CurrentGameCol - 1] < 1)
+		    {
+		      botColFlag = true;
+		    }
+		  }
+		  
+		  if(botColFlag == false)
+		  {
+		    std::cout<<"LINEDELETION";
+		    
+		     // INCREASE POINT TOTAL HERE
+
+		    for (int c=gameStateComponent->CurrentGameRow - 2; c > 0;c--)
+		    {
+		      for (int r=0; r < gameStateComponent->CurrentGameCol;r++)
+		      {
+				gameStateComponent->GameMap[r][c + 1] = gameStateComponent->GameMap[r][c];
+		      }
+		    }
+		    
+		    for (int i=0; i < gameStateComponent->CurrentGameCol; i++)
+		    {
+		      gameStateComponent->GameMap[i][0] = 0;
+		    }
+		  }
+		  
+		  //Check top row. If full: delete top row TOP.
+		  for (int r=0; r < gameStateComponent->CurrentGameCol;r++)
+		  {
+		   // std::cout<<"checcord"<<gameStateComponent->GameMap[0][r];
+
+		    if(gameStateComponent->GameMap[0][r] < 1)
+		    {
+		      topRowFlag = true;
+		    }
+		  }
+		  
+		  if(topRowFlag == false)
+		  {
+		    std::cout<<"LINEDELETION";
+		    
+		    // INCREASE POINT TOTAL HERE
+
+		    for (int c=0; c < gameStateComponent->CurrentGameRow;c++)
+		    {
+		      for (int r=1; r < gameStateComponent->CurrentGameCol;r++)
+		      {
+				gameStateComponent->GameMap[r-1][c] = gameStateComponent->GameMap[r][c];
+		      }
+		    }
+		    
+		    for (int i=0; i < gameStateComponent->CurrentGameRow; i++)
+		    {
+		      gameStateComponent->GameMap[gameStateComponent->CurrentGameRow][i] = 0;
+		    }
+		  }
+		  
+		    //Check bot row. If full: delete bot row. BOTTOM.
+		  for (int r=0; r < gameStateComponent->CurrentGameCol;r++)
+		  {
+		    if(gameStateComponent->GameMap[gameStateComponent->CurrentGameCol -1][r] < 1)
+		    {
+		      botRowFlag = true;
+		    }
+		  }
+		  
+		  if(botRowFlag == false)
+		  {
+		    std::cout<<"LINEDELETION";
+		    
+		    // INCREASE POINT TOTAL HERE
+
+		    for (int c=0; c < gameStateComponent->CurrentGameRow;c++)
+		    {
+		      for (int r=gameStateComponent->CurrentGameCol - 2; r >= 0;r--)
+		      {
+				gameStateComponent->GameMap[r+1][c] = gameStateComponent->GameMap[r][c];
+		      }
+		    }
+		    
+		    for (int i=0; i < gameStateComponent->CurrentGameRow; i++)
+		    {
+		      gameStateComponent->GameMap[0][i] = 0;
+		    }
+		  }
+		  
+		  
+		}	
+	}
+}
